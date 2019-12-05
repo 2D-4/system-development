3558adcea2187bee54af11f146df0bfd9103da09
uesb27 <kozuma036daiya@yahoo.co.jp>
2019-12-05 16:15:37 +0900

    発注書作成画面

---
+++ habigisu/habigisu/frmOrderStockCreation.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace habigisu
+{
+    partial class frmOrderStockCreation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fOSidlabel = new System.Windows.Forms.Label();
+            this.fOSnamelabel = new System.Windows.Forms.Label();
+            this.fOSupdateBtn = new System.Windows.Forms.Button();
+            this.fOSCancelBtn = new System.Windows.Forms.Button();
+            this.fOSconfirmBtn = new System.Windows.Forms.Button();
+            this.fOSbackBtn = new System.Windows.Forms.Button();
+            this.fOSDataGridview = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.fOSDataGridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fOSidlabel
+            // 
+            this.fOSidlabel.AutoSize = true;
+            this.fOSidlabel.Location = new System.Drawing.Point(597, 20);
+            this.fOSidlabel.Name = "fOSidlabel";
+            this.fOSidlabel.Size = new System.Drawing.Size(51, 15);
+            this.fOSidlabel.TabIndex = 0;
+            this.fOSidlabel.Text = "社員ID";
+            // 
+            // fOSnamelabel
+            // 
+            this.fOSnamelabel.AutoSize = true;
+            this.fOSnamelabel.Location = new System.Drawing.Point(715, 20);
+            this.fOSnamelabel.Name = "fOSnamelabel";
+            this.fOSnamelabel.Size = new System.Drawing.Size(52, 15);
+            this.fOSnamelabel.TabIndex = 1;
+            this.fOSnamelabel.Text = "社員名";
+            // 
+            // fOSupdateBtn
+            // 
+            this.fOSupdateBtn.Location = new System.Drawing.Point(12, 393);
+            this.fOSupdateBtn.Name = "fOSupdateBtn";
+            this.fOSupdateBtn.Size = new System.Drawing.Size(88, 45);
+            this.fOSupdateBtn.TabIndex = 2;
+            this.fOSupdateBtn.Text = "更新";
+            this.fOSupdateBtn.UseVisualStyleBackColor = true;
+            this.fOSupdateBtn.Click += new System.EventHandler(this.fOSupdateBtn_Click);
+            // 
+            // fOSCancelBtn
+            // 
+            this.fOSCancelBtn.Location = new System.Drawing.Point(130, 393);
+            this.fOSCancelBtn.Name = "fOSCancelBtn";
+            this.fOSCancelBtn.Size = new System.Drawing.Size(88, 45);
+            this.fOSCancelBtn.TabIndex = 3;
+            this.fOSCancelBtn.Text = "取り消し";
+            this.fOSCancelBtn.UseVisualStyleBackColor = true;
+            this.fOSCancelBtn.Click += new System.EventHandler(this.fOSCancelBtn_Click);
+            // 
+            // fOSconfirmBtn
+            // 
+            this.fOSconfirmBtn.Location = new System.Drawing.Point(579, 393);
+            this.fOSconfirmBtn.Name = "fOSconfirmBtn";
+            this.fOSconfirmBtn.Size = new System.Drawing.Size(88, 45);
+            this.fOSconfirmBtn.TabIndex = 4;
+            this.fOSconfirmBtn.Text = "確定";
+            this.fOSconfirmBtn.UseVisualStyleBackColor = true;
+            this.fOSconfirmBtn.Click += new System.EventHandler(this.fOSconfirmBtn_Click);
+            // 
+            // fOSbackBtn
+            // 
+            this.fOSbackBtn.Location = new System.Drawing.Point(700, 393);
+            this.fOSbackBtn.Name = "fOSbackBtn";
+            this.fOSbackBtn.Size = new System.Drawing.Size(88, 45);
+            this.fOSbackBtn.TabIndex = 5;
+            this.fOSbackBtn.Text = "戻る";
+            this.fOSbackBtn.UseVisualStyleBackColor = true;
+            this.fOSbackBtn.Click += new System.EventHandler(this.fOSbackBtn_Click);
+            // 
+            // fOSDataGridview
+            // 
+            this.fOSDataGridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.fOSDataGridview.Location = new System.Drawing.Point(12, 50);
+            this.fOSDataGridview.Name = "fOSDataGridview";
+            this.fOSDataGridview.RowTemplate.Height = 24;
+            this.fOSDataGridview.Size = new System.Drawing.Size(776, 337);
+            this.fOSDataGridview.TabIndex = 6;
+            // 
+            // frmOrderStockCreation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fOSDataGridview);
+            this.Controls.Add(this.fOSbackBtn);
+            this.Controls.Add(this.fOSconfirmBtn);
+            this.Controls.Add(this.fOSCancelBtn);
+            this.Controls.Add(this.fOSupdateBtn);
+            this.Controls.Add(this.fOSnamelabel);
+            this.Controls.Add(this.fOSidlabel);
+            this.Name = "frmOrderStockCreation";
+            this.Text = "frmOrderStockCreation";
+            ((System.ComponentModel.ISupportInitialize)(this.fOSDataGridview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fOSidlabel;
+        private System.Windows.Forms.Label fOSnamelabel;
+        private System.Windows.Forms.Button fOSupdateBtn;
+        private System.Windows.Forms.Button fOSCancelBtn;
+        private System.Windows.Forms.Button fOSconfirmBtn;
+        private System.Windows.Forms.Button fOSbackBtn;
+        private System.Windows.Forms.DataGridView fOSDataGridview;
+    }
+}
---
+++ habigisu/habigisu/frmOrderStockCreation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace habigisu
+{
+    public partial class frmOrderStockCreation : Form
+    {
+        OleDbConnection cn = new OleDbConnection(); //グローバル変数　コネクションオブジェクト
+        public frmOrderStockCreation()
+        {
+            InitializeComponent();
+        }
+
+        private void dataload()   //カスタム関数
+        {
+            // SQL関係はいずれ変える
+            OleDbDataAdapter da =
+                new OleDbDataAdapter("SELECT ID,Pass AS パスワード,Name AS 名前, " +
+                "PostNumber AS 郵便番号,Address AS 住所 FROM MEMBER ORDER BY ID", cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            fOSDataGridview.DataSource = dt;
+            fOSDataGridview.AllowUserToAddRows = false;    //最下行を非表示
+            fOSDataGridview.AutoResizeColumns();           //列の幅の自動調整
+        }
+
+        private void fOSupdateBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fOSCancelBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("指定された内容を削除してもよろしいですか？", "確認", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (result == DialogResult.OK)
+            {
+                int selectrow = fOSDataGridview.CurrentCell.ColumnIndex;                 //選択されている行番号
+                OleDbCommand cmd = new OleDbCommand("DELETE FROM Member WHERE 略 = @id", cn);  //ID
+                cmd.Parameters.AddWithValue("@id", fOSDataGridview.Rows[0].Cells["ID"].Value);
+                try
+                {
+                    cn.Open();                 //コネクションを開く
+                    cmd.ExecuteNonQuery();     //SQLを実行
+                    cn.Close();                //コネクションを閉じる
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    cn.Close();                //コネクションを閉じる
+                    return;
+                }
+
+                dataload();                   //データをロードする関数
+                MessageBox.Show("削除しました", "住所録");
+            }
+        }
+
+        private void fOSconfirmBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("内容を確定してよろしいですか？", "最終確認", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (result == DialogResult.OK)
+            {
+                
+                //書類生成のコードを打ち込む予定
+            }
+        }
+
+        private void fOSbackBtn_Click(object sender, EventArgs e)
+        {
+            //this.Hide();
+            //frmOrderStockManegement.Show();
+        }
+    }
+}
