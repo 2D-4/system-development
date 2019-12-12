20d62c3af03077fc80009e7cbe2d720ea5b5c5c7
nakamotoD <b8270@oic.jp>
2019-12-12 16:22:08 +0900

    データベース接続

--- habigisu/habigisu/frmShippingManagement.cs
+++ habigisu/habigisu/frmShippingManagement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace habigisu
 {
     public partial class frmShippingManagement : Form
     {
+        OleDbConnection cn = new OleDbConnection(); //グローバル変数　コネクションオブジェクト
         public frmShippingManagement()
         {
             InitializeComponent();
@@ -23,24 +25,53 @@ namespace habigisu
 
         }
 
+        private void dataload()   //カスタム関数
+        {
+            cn.ConnectionString =
+                @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Habigisu.accdb;";
+        }
+
         private void FSMSlistBtn_Click(object sender, EventArgs e)
         {
-        // ("SELECT *　from  テーブル名Issue ") 出庫一覧
+            //出庫一覧
+            dataload();
+            OleDbDataAdapter da =
+                new OleDbDataAdapter("SELECT ID,Pass AS 出庫ID,Name AS 出庫日, " +
+                "PostNumber AS 社員ID,Address AS 注文ID FROM MEMBER ORDER BY ID", cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            FSMDatagridview.DataSource = dt;
+            FSMDatagridview.AllowUserToAddRows = false;    //最下行を非表示
+            FSMDatagridview.AutoResizeColumns();           //列の幅の自動調整
         }
 
         private void FSMNotSlistBtn_Click(object sender, EventArgs e)
         {
-            //未出庫一覧
+            // 未出庫一覧
+            dataload();
+            OleDbDataAdapter da =
+                new OleDbDataAdapter("SELECT ID,Pass AS 出庫ID,Name AS 出庫日, " +
+                "PostNumber AS 社員ID,Address AS 注文ID FROM MEMBER ORDER BY ID", cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            FSMDatagridview.DataSource = dt;
+            FSMDatagridview.AllowUserToAddRows = false;    //最下行を非表示
+            FSMDatagridview.AutoResizeColumns();           //列の幅の自動調整
         }
 
         private void FSMSearchBtn_Click(object sender, EventArgs e)
         {
-            //検索
+            //strFilter　検索
         }
 
         private void FSMBackBtn_Click(object sender, EventArgs e)
         {
             this.Close(); //閉じる
+        }
+
+        private void frmShippingManagement_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
