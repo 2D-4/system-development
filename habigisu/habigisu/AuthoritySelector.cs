6c1d479441248fc4c12918f34ab24c254c456dfb
natsu-cat <tetsu19980416@gmail.com>
2020-01-14 16:28:55 +0900

    権限で管理画面への遷移を制限する関数AuthoritySelectorを作成した。

---
+++ habigisu/habigisu/AuthoritySelector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace habigisu
+{
+    //役職ｺｰﾄﾞ、部署ｺｰﾄﾞの3桁目で判断し、レベル毎に閲覧を制限するクラス
+    class AuthoritySelector
+    {
+        OleDbConnection cn = new OleDbConnection(); //グローバル変数　コネクションオブジェクト
+
+        //管理毎にレベルに応じて閲覧を制限する関数
+        public int CheckDeptAuthority(int ManageNumber, string eid)
+        {
+            cn.ConnectionString =
+                @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\habigisu.accdb;";
+
+            OleDbDataAdapter da = new OleDbDataAdapter(); //データアダプタオブジェクト
+            OleDbCommand cmd = new OleDbCommand();        //コマンドオブジェクト
+            cmd.Connection = cn;
+            cn.Open();
+
+            cmd.CommandText = "SELECT 部署ｺｰﾄﾞ FROM 社員マスタ WHERE ID=@id"; //部署ｺｰﾄﾞをDBから取得するSQL文
+            da.SelectCommand = cmd;
+
+            cmd.Parameters.AddWithValue("@id", eid);        //IDのパラメータ
+            DataTable dt = new DataTable();
+                da.Fill(dt);
+            if (dt.Rows.Count > 0)                           //検索結果が正常に帰ってきた時
+            {
+                string dcode = cmd.ExecuteScalar().ToString();
+                char d1 = dcode[0];
+                switch (ManageNumber)
+                {
+                    case 0:
+                        if (d1 == '4')
+                            return 0;
+                        break;
+                    case 1:
+                        if (d1 == '3' || d1 == '4')
+                            return 0;
+                        break;
+                    case 2:
+                        if (d1 == '3' || d1 == '4')
+                            return 0;
+                        break;
+                    default:
+                        break;
+                }
+                return -1;
+            }
+            else
+                return -1;
+        }
+
+    }
+}
---
+++ habigisu/habigisu/frmMain.cs
@@ -0,0 +1,62 @@
+﻿using habigisu;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace habigisu
+{
+    public partial class frmMain : Form
+    {
+        public AuthoritySelector au = new AuthoritySelector();
+        public frmMain()
+        {
+            InitializeComponent();           
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOderStock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShipping_Click(object sender, EventArgs e)
+        {
+            au.CheckDeptAuthority(0, eid);
+        }
+
+        private void btnSales_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPassChange_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
